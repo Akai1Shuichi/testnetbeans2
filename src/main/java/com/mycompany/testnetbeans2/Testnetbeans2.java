e3872ed7f10bc404e5fdf7186206afda868b17cc
trongtoan <toancccccccc@gmail.com>
2023-03-26 17:24:28 +0700

    ok

---
+++ src/main/java/com/mycompany/testnetbeans2/Testnetbeans2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testnetbeans2;
+
+/**
+ *
+ * @author toanc
+ */
+public class Testnetbeans2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
